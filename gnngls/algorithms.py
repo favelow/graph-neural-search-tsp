a8766e999777bf00509f598074067c1d421e3972
favelow <158656528+favelow@users.noreply.github.com>
2023-07-19 08:56:09 +0000

    New translations app.dev.po (Spanish)

--- gnngls/algorithms.py
+++ gnngls/algorithms.py
@@ -6,4 +6,7 @@ import numpy as np
 from . import tour_cost, operators
 
 
-def nearest_neighbor(G, depo
+def nearest_neighbor(G, depot, weight='weight'):
+    tour = [depot]
+    while len(tour) < len(G.nodes):
+        i = tour[-1]
