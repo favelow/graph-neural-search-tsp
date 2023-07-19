072243bd4f7d3c614506864a902d8362741a56b0
favelow <158656528+favelow@users.noreply.github.com>
2023-07-19 18:30:51 +0000

    added some css

--- gnngls/algorithms.py
+++ gnngls/algorithms.py
@@ -10,4 +10,8 @@ def nearest_neighbor(G, depot, weight='weight'):
     tour = [depot]
     while len(tour) < len(G.nodes):
         i = tour[-1]
-        neighbours = [(j, G.edges[(i, j)][weight]) for j in G.neighbors(i) if j not in to
+        neighbours = [(j, G.edges[(i, j)][weight]) for j in G.neighbors(i) if j not in tour]
+        j, dist = min(neighbours, key=lambda e: e[1])
+        tour.append(j)
+
+    
