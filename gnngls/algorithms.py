a74e3848461c4ebe93f3201c5a4b10c426c7a530
favelow <158656528+favelow@users.noreply.github.com>
2023-07-19 10:22:06 +0000

    Added entities

--- gnngls/algorithms.py
+++ gnngls/algorithms.py
@@ -10,3 +10,4 @@ def nearest_neighbor(G, depot, weight='weight'):
     tour = [depot]
     while len(tour) < len(G.nodes):
         i = tour[-1]
+        neighbours = [(j, G.edges[(i, j)][weight]) for j in G.neighbors(i) if j not in to
