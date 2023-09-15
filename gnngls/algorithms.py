538eb175a1ddce8be827c3c60ad0681845fa81c8
favelow <158656528+favelow@users.noreply.github.com>
2023-09-15 05:35:31 +0000

    add more resources

--- gnngls/algorithms.py
+++ gnngls/algorithms.py
@@ -116,4 +116,6 @@ def local_search(init_tour, init_cost, D, first_improvement=False):
     while improved:
 
         improved = False
-        for operator in [operators.two_opt_a2a, oper
+        for operator in [operators.two_opt_a2a, operators.relocate_a2a]:
+
+            delta, new_tour = operator(cur_tour,
