03521a43b324e238c187f2962372ef79a2b7623c
favelow <158656528+favelow@users.noreply.github.com>
2023-10-13 05:47:22 +0000

    Fix a few typos

--- gnngls/algorithms.py
+++ gnngls/algorithms.py
@@ -156,4 +156,8 @@ def guided_local_search(G, init_tour, init_cost, t_lim, weight='weight', guides=
                 util = G.edges[e][guide] / (1 + G.edges[e]['penalty'])
                 if util > max_util or max_util_e is None:
                     max_util = util
- 
+                    max_util_e = e
+
+            G.edges[max_util_e]['penalty'] += 1.
+
+            edge_
