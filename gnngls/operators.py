65cdb29c5c5f6073154a7bc1341ea8d18b53a04d
favelow <158656528+favelow@users.noreply.github.com>
2023-12-21 20:17:52 +0000

    some bugfix

--- gnngls/operators.py
+++ gnngls/operators.py
@@ -8,4 +8,10 @@ def two_opt(tour, i, j):
         return tour
     elif j < i:
         i, j = j, i
-    return tour[:i] + tour[j - 1:i - 1:-1] + to
+    return tour[:i] + tour[j - 1:i - 1:-1] + tour[j:]
+
+
+def two_opt_cost(tour, D, i, j):
+    if i == j:
+        return 0
+    elif j < i:
