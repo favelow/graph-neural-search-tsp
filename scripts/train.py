04e0930ba5ae8e636d7eb58a2a8c96dda13de9a2
favelow <158656528+favelow@users.noreply.github.com>
2024-03-01 17:42:50 +0000

    Finished README

--- scripts/train.py
+++ scripts/train.py
@@ -60,3 +60,105 @@ def test(model, data_loader, target, criterion, device):
 
 def save(model, optimizer, epoch, train_loss, val_loss, save_path):
     torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': train_loss,
+        'val_loss': val_loss
+    }, save_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train model')
+    parser.add_argument('data_dir', type=pathlib.Path, help='Where to load dataset')
+    parser.add_argument('tb_dir', type=pathlib.Path, help='Where to log Tensorboard data')
+    parser.add_argument('--embed_dim', type=int, default=128, help='Maximum hidden feature dimension')
+    parser.add_argument('--n_layers', type=int, default=3, help='Number of message passing steps')
+    parser.add_argument('--n_heads', type=int, default=8, help='Number of attention heads for GAT')
+    parser.add_argument('--lr_init', type=float, default=1e-3, help='Initial learning rate')
+    parser.add_argument('--lr_decay', type=float, default=0.99, help='Learning rate decay')
+    parser.add_argument('--min_delta', type=float, default=1e-4, help='Early stopping min delta')
+    parser.add_argument('--patience', type=int, default=20, help='Early stopping patience')
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
+    parser.add_argument('--n_epochs', type=int, default=100, help='Number of epochs')
+    parser.add_argument('--checkpoint_freq', type=int, default=None, help='Checkpoint frequency')
+    parser.add_argument('--target', type=str, default='regret', choices=['regret', 'in_solution'])
+    parser.add_argument('--use_gpu', action='store_true')
+    args = parser.parse_args()
+
+    # Load dataset
+    train_set = datasets.TSPDataset(args.data_dir / 'train.txt')
+    val_set = datasets.TSPDataset(args.data_dir / 'val.txt')
+
+    # use GPU if it is available
+    device = torch.device('cuda' if args.use_gpu and torch.cuda.is_available() else 'cpu')
+    print('device =', device)
+
+    _, feat_dim = train_set[0].ndata['features'].shape
+
+    model = models.EdgePropertyPredictionModel(
+        feat_dim,
+        args.embed_dim,
+        1,
+        args.n_layers,
+        n_heads=args.n_heads,
+    ).to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr_init)
+    lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, args.lr_decay)
+
+    if args.target == 'regret':
+        criterion = torch.nn.MSELoss()
+
+    elif args.target == 'in_solution':
+        # only works for a homogenous dataset
+        y = train_set[0].ndata['in_solution']
+        pos_weight = len(y) / y.sum() - 1
+        criterion = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, collate_fn=dgl.batch,
+                              num_workers=os.cpu_count())
+    val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=True, collate_fn=dgl.batch,
+                            num_workers=os.cpu_count())
+
+    timestamp = datetime.datetime.now().strftime('%b%d_%H-%M-%S')
+    run_name = f'{timestamp}_{uuid.uuid4().hex}'
+    log_dir = args.tb_dir / run_name
+    writer = SummaryWriter(log_dir)
+
+    # early stopping
+    best_score = None
+    counter = 0
+
+    pbar = tqdm.trange(args.n_epochs)
+    for epoch in pbar:
+        epoch_loss = train(model, train_loader, args.target, criterion, optimizer, device)
+        writer.add_scalar("Loss/train", epoch_loss, epoch)
+
+        epoch_val_loss = test(model, train_loader, args.target, criterion, device)
+        writer.add_scalar("Loss/validation", epoch_val_loss, epoch)
+
+        pbar.set_postfix({
+            'Train Loss': '{:.4f}'.format(epoch_loss),
+            'Validation Loss': '{:.4f}'.format(epoch_val_loss),
+        })
+
+        if args.checkpoint_freq is not None and epoch > 0 and epoch % args.checkpoint_freq == 0:
+            checkpoint_name = f'checkpoint_{epoch}.pt'
+            save(model, optimizer, epoch, epoch_loss, epoch_val_loss, log_dir / checkpoint_name)
+
+        if best_score is None or epoch_val_loss < best_score - args.min_delta:
+            save(model, optimizer, epoch, epoch_loss, epoch_val_loss, log_dir / 'checkpoint_best_val.pt')
+
+            best_score = epoch_val_loss
+            counter = 0
+        else:
+            counter += 1
+
+        if counter >= args.patience:
+            pbar.close()
+            break
+
+        lr_scheduler.step()
+
+    writer.close()
