6497459ba668b99a8b0d3062f8badb31baefc64f
favelow <158656528+favelow@users.noreply.github.com>
2024-03-07 19:55:42 +0000

    Update Installation.md

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
 
 setup(name='gnngls',
-      description='Graph Neura
+      description='Graph Neural Network Guided Local Search for the Traveling Salesperson Problem',
+      author='Ben Hudson',
+    
