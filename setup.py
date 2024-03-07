aa9a83f759e263c7338aa773571c949dbe136432
favelow <158656528+favelow@users.noreply.github.com>
2024-03-07 21:12:16 +0000

    Prepare 1.1.5 release

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ from setuptools import setup
 setup(name='gnngls',
       description='Graph Neural Network Guided Local Search for the Traveling Salesperson Problem',
       author='Ben Hudson',
-    
+      author_email='benhudson@fastmail.com',
+      version='0.01',
+      packages=['g
